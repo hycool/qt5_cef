1b2f4dee4e0937d8e72caf5e7f59a4657dd60b1e
HuaiYang <huai.y@syman.cn>
2018-09-06 09:27:30 +0800

    '增加键盘输入法测试脚本'

---
+++ test/input.py
@@ -0,0 +1,44 @@
+# coding:cp936
+
+# by 书记: bh2#qq.com
+# do what you want to do with this script
+# pywin32 needed
+
+import win32con
+import win32api
+
+
+class InputMethod(list):
+    def __init__(self):
+        name = "Keyboard Layout\\Preload"
+        _k_id = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, name)
+        print('_k_id = ', _k_id)
+
+        name = "System\\CurrentControlSet\\Control\\Keyboard Layouts\\"
+        _i, _running, _ids = 1, True, list()
+        while _running:
+            try:
+                _id = win32api.RegQueryValueEx(_k_id, str(_i))[0]
+                _i += 1
+                _k_name = win32api.RegOpenKey(win32con.HKEY_LOCAL_MACHINE, name + _id)
+                _name = win32api.RegQueryValueEx(_k_name, 'Layout Text')[0]
+                self.append((_id, _name))
+                win32api.RegCloseKey(_k_name)
+            except:
+                _running = False
+                win32api.RegCloseKey(_k_id)
+
+    def set(self, _im):
+        print('_im[0] = ', _im[0], type(_im[0]))
+        win32api.LoadKeyboardLayout(_im[0], win32con.KLF_ACTIVATE)
+
+
+if __name__ == '__main__':
+    im = InputMethod()
+    for i, v in enumerate(im):
+        print('【输入法%d】: %s' % (i, v[1]))
+    i = input('设置输入法(输入数字并回车): ')
+    print(type(i), i)
+    im.set(im[int(i)])
+    print('成功将输入法设置为【%s】' % im[int(i)][1])
+    i = input('随便输入点什么：')
